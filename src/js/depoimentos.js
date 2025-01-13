9cd9c4324a10697617953485a7c7394c754fa870
Robert <robertrdossantos317@gmail.com>
2025-01-13 07:56:21 -0300

    add depoimentos

--- src/js/depoimentos.js
+++ src/js/depoimentos.js
@@ -1,8 +1,8 @@
-const content = document.getElementById('depoimentos-content');
+const content = document.getElementById('depoimentos-content-p');
 const btnNext = document.getElementById('bttn-next');
 const btnBack = document.getElementById('bttn-back');
 
-const cardWidth = content.offsetWidth / 2; // Calcula a largura de dois depoimentos visíveis
+const cardWidth = content.offsetWidth / 1; // Calcula a largura de dois depoimentos visíveis
 
 // Função para avançar
 btnNext.addEventListener('click', () => {
