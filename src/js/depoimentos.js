59c6131ee6a7e5e88fd4823e227622b61abc302b
Robert <robertrdossantos317@gmail.com>
2025-01-14 01:00:13 -0300

    responsive home.css

--- src/js/depoimentos.js
+++ src/js/depoimentos.js
@@ -1,15 +1,31 @@
-const content = document.getElementById('depoimentos-content-p');
+const content = document.getElementById('depoimentos-content');
 const btnNext = document.getElementById('bttn-next');
 const btnBack = document.getElementById('bttn-back');
+const cardWidth = content.offsetWidth / 1;
 
-const cardWidth = content.offsetWidth / 1; // Calcula a largura de dois depoimentos visíveis
-
-// Função para avançar
 btnNext.addEventListener('click', () => {
-    content.scrollLeft += cardWidth; // Avança dois depoimentos
+    content.scrollLeft += cardWidth; 
+});
+btnBack.addEventListener('click', () => {
+    content.scrollLeft -= cardWidth; 
 });
 
-// Função para voltar
+
+
+const content2 = document.getElementById('depoimentos-content-p');
+const cardWidth2 = content2.offsetWidth / 1; 
+
+btnNext.addEventListener('click', () => {
+    content2.scrollLeft += cardWidth; 
+});
 btnBack.addEventListener('click', () => {
-    content.scrollLeft -= cardWidth; // Volta dois depoimentos
+    content2.scrollLeft -= cardWidth2;
+});
+
+
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("header-menu");
+
+hamburger.addEventListener("click", () => {
+  menu.classList.toggle("active");
 });
