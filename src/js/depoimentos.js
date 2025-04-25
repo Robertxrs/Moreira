a7a8c767181e03881eaacba48295f48d99adc43f
Robert <robertrdossantos317@gmail.com>
2025-04-25 15:29:29 -0300

    updtate dopoimentos

--- src/js/depoimentos.js
+++ src/js/depoimentos.js
@@ -37,3 +37,12 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
+const buttons = document.querySelectorAll('.depoimentos-buttons-p button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        buttons.forEach(btn => btn.classList.remove('active'));
+        button.classList.add('active');
+    });
+});
+
